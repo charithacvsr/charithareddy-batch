f1b430dda630d7d46b88a78aa4c8fcfe7b8ba4a7
charithacvsr <162982659+charithacvsr@users.noreply.github.com>
2024-03-18 11:52:58 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,121 @@
+###day7##
+'''
+d1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+d2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+d1.update(d2)
+print(d1)
+
+'''
+'''
+ set1 = {'Python', 'Java', 'Data Science'}
+ set2 = {'ML', 'AI', 'R Language', 'Python'}
+ set3 = {'Data Analytics', 'Robotics', 'Deep L
+ c = 0
+ flag=0       
+     for val in range(3):
+         c=c+1
+         if c==1:
+         for val in set1:
+             if val in set2 or setl in set3:
+                  flag=1
+                  break
+
+       if c==2:
+         for val in set1:
+             if val in  setl in set3:
+                  flag=1
+                  break
+
+       if c==3:
+         for val in set1:
+             if val in set2 or setl :
+                  flag=1
+                  break
+
+if flag==0:
+    print("disjoint")
+else:
+    print("joint")
+
+
+'''
+'''
+list
+1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+l3 =[]
+for val in range(len(list1)):
+    ans = list[val]+list2[val]
+    l3.appened(ans)
+print(l3)       
+       
+'''
+
+
+'''
+
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+i=0
+while i<len(list1):
+    13.append(list1[i]+list2[i])
+    i=+1
+print(l3)    
+'''
+
+
+
+ '''
+# ! Functions
+# Defination
+# Function is a block of code which is used to perform a specific
+# functionality
+# Function can be created using def keyword
+
+# Function has 3 parts
+# Function declaration
+# Function defination
+# Function cll
+'''
+ '''
+# @!  eg:1
+def greet(): # *function defination
+    print("welcome user!!")
+ 
+
+greet()   
+greet()   
+greet()   
+greet() # function calling
+'''
+ '''
+# ! eg:2
+def adding():
+    d=a+b+c
+    print(d)
+adding()    
+adding() 
+
+
+def greeting(name):
+    print("welcome",name)
+    
+for val in range (3):
+    username+input("enter the name:")
+greting(username)# username is argument
+'''
+
+# eg:3
+
+
+
+
+
+
+
+
+
+                
+
+       
